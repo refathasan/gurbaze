e3c84103dc273c9d7b5e73df53299185bda5cf88
Refat Hasan <refat.hasan@bjitgroup.com>
2016-09-29 14:11:45 +0600

    Second commit of git
    c++ commit 2

--- file1.cpp
+++ file1.cpp
@@ -4,5 +4,12 @@ using namespace std;
 int main()
 {
 	cout<<"Hello World"<<endl;
+	cout<<"Welcome to git"<<endl;
+
+	for (int i  = 0; i<=10; i++)
+	{
+		cout<<"*";
+		cout<< " "<<endl;
+	}
 	return 0;
 }
